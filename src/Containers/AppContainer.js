7c3045fdf2c147ebf8fe9b5668cced3326346b51
github username <github email address>
2017-09-21 17:44:58 -0400

    started synapse-ai

---
+++ src/Containers/AppContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div >
+        In App Container
+      </div>
+    );
+  }
+}
+
+export default AppContainer
